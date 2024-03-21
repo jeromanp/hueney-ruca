50986030ec74b4a8bc30eec331e9fd8e164ea85a
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-03-20 20:15:41 -0600

    add metadata title favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,54 +1,53 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.googleapis.com"
-                    ></link>
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.gstatic.com"
-                        crossOrigin="true"
-                    ></link>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                        rel="stylesheet"
-                    ></link>
-                    <link
-                        href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css"
-                        rel="stylesheet"
-                    ></link>
-                    {/* inicio de links de react-slider */}
-                    <link
-                        rel="stylesheet"
-                        type="text/css"
-                        charSet="UTF-8"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-                    />
-                    <link
-                        rel="stylesheet"
-                        type="text/css"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-                    />
-                    {/* fin de links de react-slider */}
-                </Head>
-                <body className="antialiased font-poppins">
-                    <Main />
-                    <NextScript />
-                    <div id="portal" />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Hueney Ruca</title>
+          <link rel="icon" href="/brand.svg" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css"
+            rel="stylesheet"
+          ></link>
+          {/* inicio de links de react-slider */}
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+          {/* fin de links de react-slider */}
+        </Head>
+        <body className="antialiased font-poppins">
+          <Main />
+          <NextScript />
+          <div id="portal" />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
